3e87cd02a5bf06f4daa53823d9716d70d3c44bb1
guoming <geniusming@qq.com>
2019-04-09 15:18:03 +0800

    update

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@ mkdir -p /mnt/elasticsearch/data
 chmod 777 /mnt/elasticsearch/data
 sysctl -w vm.max_map_count=262144
 
+
+docker node update --label-add elasticsearch=node1 sz-new-test
+docker node update --label-add elasticsearch=node2 sz-itdev4
+
 docker stack deploy --compose-file docker-compose.yml elasticsearch
